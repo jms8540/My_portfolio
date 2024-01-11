67cba3a3d9419e594a7ba489761ef87abef5620d
jeonminsu <j01085402702@gmail.com>
2024-01-11 20:56:50 +0900

    save+
    이제 크롤링 값 띄우기 밑
    한페이지 남음
    (디자인 보수)

---
+++ mealsMenu/script.js
@@ -0,0 +1,8 @@
+// const selectBt = document.querySelectorAll("#select_deleteBt")
+// const selectTtem = document.querySelectorAll(".item")
+
+// selectBt.forEach((e, idx)=>{
+//     e.addEventListener("click", ()=>{
+//         selectTtem[idx].remove()
+//     })
+// })
